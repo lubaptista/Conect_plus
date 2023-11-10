9a38819116d69ac04c9a05111f458ce9df710c21
Daniela Hayashi <107941355+danchih@users.noreply.github.com>
2023-11-10 15:50:51 -0300

    Cadastro e Login
    Cadastro e login estão conectados

--- js/login.js
+++ js/login.js
@@ -3,12 +3,21 @@ function logar()
 {
     var email = document.getElementById("email");
     var password = document.getElementById("password");
+    var inexistente = true;
 
-    if(email.value == "teste@teste.com" && password.value == "1234"){
-        localStorage.setItem("acesso", true);
+    if(localStorage.cadastro){
+        dados = JSON.parse(localStorage.getItem("cadastro"));
+    }
+    for(var i in dados){
+        if(email.value == dados[i].email && password.value == dados[i].password){
+            localStorage.setItem("acesso", true);
+    
+            window.location.href = "home.html";
+            inexistente = false;
+        }
+    }
 
-        window.location.href = "home.html";
-    }else{
+    if(inexistente == true){
         alert("Usuário ou senha inválidos!");
     }
 
